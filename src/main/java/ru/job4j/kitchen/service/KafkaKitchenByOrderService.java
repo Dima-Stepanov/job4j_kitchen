fb73ec60bb0acd703009f743b26778fed8066447
dmitryjob4j <haoos@inbox.ru>
2023-05-12 15:46:14 +0300

    3. Spring boot + Kafka [#505039]

--- src/main/java/ru/job4j/kitchen/service/KafkaKitchenByOrderService.java
+++ src/main/java/ru/job4j/kitchen/service/KafkaKitchenByOrderService.java
@@ -1,6 +1,5 @@
 package ru.job4j.kitchen.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
@@ -20,9 +19,8 @@ import ru.job4j.kitchen.domain.dto.OrderDTO;
 @Service
 @AllArgsConstructor
 @Slf4j
-public class KafkaKitchenByOrderService implements KafkaService<String, String, OrderDTO> {
+public class KafkaKitchenByOrderService implements KafkaService<String, OrderDTO, OrderDTO> {
     private static final String TOPIC_ORDERS = "job4j_orders";
-    private final ObjectMapper objectMapper;
 
     @Override
     public void sendMessage(String topic, String key, OrderDTO type) {
@@ -32,16 +30,10 @@ public class KafkaKitchenByOrderService implements KafkaService<String, String, 
 
     @KafkaListener(topics = TOPIC_ORDERS)
     @Override
-    public OrderDTO receive(ConsumerRecord<String, String> record) {
+    public OrderDTO receive(ConsumerRecord<String, OrderDTO> record) {
         log.debug("Partition: {}", record.partition());
         log.debug("Key: {}", record.key());
         log.debug("Value: {}", record.value());
-        OrderDTO orderDTO;
-        try {
-            orderDTO = objectMapper.readValue(record.value(), OrderDTO.class);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        return orderDTO;
+        return record.value();
     }
 }
